e1191dc82b8efac58472f391002fa3afb1b2b6c0
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-02 17:53:22 +0200

    #1 Oltás osztály létrehozása

---
+++ AllatOrvosiNyilvantarto/Oltas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllatOrvosiNyilvantarto
+{
+    class Oltas
+    {
+		public Oltas(uint Id, string Nev, OltasAllat Allat)
+		{
+			this.Id = Id;
+			this.Nev = Nev;
+			this.Allat = Allat;
+		}
+
+		private uint id;
+
+		public uint Id
+		{
+			get { return id; }
+			set
+			{
+				if (value < 1)
+					throw new Exception("Az azonosító nem lehet 1-nél kisebb!");
+				id = value;
+			}
+		}
+		private string nev;
+
+		public string Nev
+		{
+			get { return nev; }
+			set
+			{
+				if (value == null)
+					throw new Exception("A név nem lehet null!");
+				if (value.Length < 1)
+					throw new Exception("A név hossza legalább 1!");
+				nev = value;
+			}
+		}
+
+		public OltasAllat Allat { get; set; }
+	}
+}
