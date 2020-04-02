d9e96a4b7d1e6296fdb4d4ac8a25a7ca7e39db33
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-02 21:11:40 +0200

    #1 Tulajdonos konténerosztály létrehozása

---
+++ AllatOrvosiNyilvantarto/Classes/Tulajdonosok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace AllatOrvosiNyilvantarto.Classes
+{
+    static class Tulajdonosok
+    {
+		private static uint ID = 0;
+
+		private static List<Tulajdonos> tulajdonosok = new List<Tulajdonos>();
+
+		public static List<Tulajdonos> TulajdonosokLista
+		{
+			get { return new List<Tulajdonos>(tulajdonosok); }
+		}
+
+		public static void TulajdonosFelvetele(string Vnev, string Knev, DateTime SzulIdo, string Irszam, string Varos, string Lakcim, string MobilSzam)
+		{
+			tulajdonosok.Add(new Tulajdonos(++ID, Vnev, Knev, SzulIdo, Irszam, Varos, Lakcim, MobilSzam));
+		}
+		public static void TulajdonosFelvetele(string Vnev, string Knev, DateTime SzulIdo, string Irszam, string Varos, string Lakcim, string MobilSzam, string Email)
+		{
+			tulajdonosok.Add(new Tulajdonos(++ID, Vnev, Knev, SzulIdo, Irszam, Varos, Lakcim, MobilSzam, Email));
+		}
+	}
+}
